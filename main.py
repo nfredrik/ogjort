258bc90058afe5b2e372608f4be98da05d73438b
fredrik <nfredrik@hotmail.com>
2024-05-01 21:07:05 +0200

    reformatted

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from datetime import date, timedelta
 from midsummer import midsummer_date as midsommar
+
 app = FastAPI()
 
 
@@ -11,7 +12,9 @@ async def read_midsummer(year: int):
     june_twentieth = date(year, 6, 20)
 
     # Find the Saturday following June 20th
-    midsummer_date = june_twentieth + timedelta(days=(5 - june_twentieth.weekday() + 7) % 7)
+    midsummer_date = june_twentieth + timedelta(
+        days=(5 - june_twentieth.weekday() + 7) % 7
+    )
 
     # Check if the calculated date is beyond June 26th
     if midsummer_date.day > 26:
@@ -19,7 +22,6 @@ async def read_midsummer(year: int):
         midsummer_date -= timedelta(days=7)
 
     return {"Midsummer Date": midsommar(year=year)}
-
 
 
 if __name__ == "__main__":
